2f828cab95da39b79cdb60859cca97e69504a713
Leliya <98910653+Leliya@users.noreply.github.com>
2022-09-12 18:02:42 +0400

    feature: доработать модель пользователя

--- models/user.js
+++ models/user.js
@@ -1,21 +1,35 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
+  },
+  email: {
+    type: String,
     required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Введенные данные не являются Email.',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
   },
 });
 
